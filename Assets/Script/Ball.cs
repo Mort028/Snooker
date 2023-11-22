781ca8983648e6996200b2797e1d33e2981d0b71
Mort028 <fpp.2288@gmail.com>
2023-11-23 00:21:15 +0700

    Referee set ball
    set ball on table with set color

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -2,17 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BallColors
+{
+    White,
+    Red,
+    Yellow,
+    Green,
+    Brown,
+    Blue,
+    Pink,
+    Black
+}
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private BallColors ballColor;
+    [SerializeField] MeshRenderer rd;
+    [SerializeField] private int point;
+    public int Point { get { return point; } }
+
+    public void Awake()
     {
-        
+        rd = GetComponent<MeshRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetColorAndPoint(BallColors color)
     {
-        
+        switch (color)
+        {
+            case BallColors.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColors.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColors.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColors.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColors.Brown:
+                point = 4;
+                rd.material.color = new Color32(145, 81, 9, 255);
+                break;
+            case BallColors.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColors.Pink:
+                point = 6;
+                rd.material.color = Color.magenta;
+                break;
+            case BallColors.Black:
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+        }
     }
 }
