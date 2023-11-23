8959dfefee579695a6237df848050b2471f89121
Fpt28 <pongpalin.trak@bumail.net>
2023-11-23 16:44:34 +0700

    reset camera to ball
    backspace to reset camera and active line again

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -20,6 +20,7 @@ public class GameManager : MonoBehaviour
         instance = this;
 
         camera = Camera.main.gameObject;
+        CameraSet();
 
         //set ball on table
         SetBalls(BallColors.White, 0);
@@ -41,12 +42,19 @@ public class GameManager : MonoBehaviour
         {
             ShootBall();
         }
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            StopBall();
+        }
     }
     void SetBalls(BallColors color, int pos)
     {
         GameObject ball = Instantiate(ballPrefab, ballPositions[pos].transform.position, Quaternion.identity);
         Ball scriptBall = ball.GetComponent<Ball>();
         scriptBall.SetColorAndPoint(color);
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.drag = 1;
     }
 
     void RotateBall()
@@ -57,9 +65,27 @@ public class GameManager : MonoBehaviour
 
     void ShootBall()
     {
+        camera.transform.parent = null;
         Rigidbody rigidbody = cueBall.GetComponent<Rigidbody>();
         rigidbody.AddRelativeForce(Vector3.forward*forceBall,ForceMode.Impulse);
         ballLine.SetActive(false);
     }
-    
+
+    void CameraSet()
+    {
+        camera.transform.parent = cueBall.transform.transform;
+        camera.transform.position = cueBall.transform.position + new Vector3(0f, 8f, -6f);
+    }
+
+    void StopBall()
+    {
+        Rigidbody rigidbody = cueBall.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        
+        cueBall.transform.eulerAngles = Vector3.zero;
+        CameraSet();
+        camera.transform.eulerAngles = new Vector3(40f, 0f, 0f);
+        ballLine.SetActive(true);
+    }
 }
